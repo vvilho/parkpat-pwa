0732e2d315a0f9408feaed5fba3c26606989dac8
Vilho Voutilainen <vilho.voutilainen@metropolia.fi>
2021-11-26 09:41:00 +0200

    sunset functionality off :(
     metrici works too good

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -49,9 +49,9 @@ let theme = createTheme({
             fontFamily: "Heebo",
         },
         h7: {
-          fontFamily: 'Roboto',
+            fontFamily: 'Roboto',
         },
-        button:{
+        button: {
             fontFamily: "Heebo",
         },
         fontFamily: "Roboto",
@@ -61,143 +61,158 @@ let theme = createTheme({
 theme = responsiveFontSizes(theme);
 
 const LandingPage = () => {
+        /**
+         * if you want to put on 'app off after sunset'-functionality, set variable "sunsetFuncOn" true
+         * also add conditional rendering with variable 'sunHasSet'
+         * to render <SunsetChecker sunriseUnix={sunrise}/> if true and normal code otherwise about on row 212
+         *
+         * And lastly uncomment two sunsetfunc useEffect()s from about row 150. Search #sunsetfunc with ctrl+f to find them
+         *
+         * @type {boolean} sunsetFuncOn
+         */
+
+        const sunsetFuncOn = false
 
 
-    const [parkingState, setParkingState] = useState(defaultState);
-    const [totalFreeSpaces, setTotalFreeSpaces] = useState(0);
-    const [freeSpacesText, setFreeSpacesText] = useState({});
-    const [freeInvaSpaces, setFreeInvaSpaces] = useState(0);
-    const [invaSpacesText, setInvaSpacesText] = useState({});
-    const [screenWidth, setScreenWidth] = useState(true);
-    const [freeNormalSpaces, setFreeNormalSpaces] = useState(0);
-    const [loading, setLoading] = useState(true);
-    const [sunset, setSunset] = useState();
-    const [sunrise, setSunrise] = useState();
-    const [timeNow, setTimeNow] = useState();
-    const [sunHasSet, setSunHasSet] = useState(false);
+        const [parkingState, setParkingState] = useState(defaultState);
+        const [totalFreeSpaces, setTotalFreeSpaces] = useState(0);
+        const [freeSpacesText, setFreeSpacesText] = useState({});
+        const [freeInvaSpaces, setFreeInvaSpaces] = useState(0);
+        const [invaSpacesText, setInvaSpacesText] = useState({});
+        const [screenWidth, setScreenWidth] = useState(true);
+        const [freeNormalSpaces, setFreeNormalSpaces] = useState(0);
+        const [loading, setLoading] = useState(true);
+        const [sunset, setSunset] = useState();
+        const [sunrise, setSunrise] = useState();
+        const [timeNow, setTimeNow] = useState();
+        const [sunHasSet, setSunHasSet] = useState(false);
 
 
-
-
-
-    // Initial call for fetch functions
-    useEffect(() => {
-        asyncFetch();
-        fetchSunset();
-        setTimeNow(Date.now()/1000);
-
-
-        const width = window.innerWidth;
-
-        if (width > 599) {
-            setScreenWidth(false);
-        }
-    }, []);
-
-
-    // Check if inva spaces are occupied and show them separately
-    useEffect(() => {
-        const list = [parkingState.ID20, parkingState.ID27];
-        const count = list.filter(item => !item).length;
-        setFreeInvaSpaces(count);
-
-        if (count === 2) {
-            setInvaSpacesText({text: 'kaksi paikkaa vapaana', style: 'green'});
-        } else if (count === 1) {
-            setInvaSpacesText({text: 'yksi paikka vapaana', style: '#e7d213'});
-        } else {
-            setInvaSpacesText({text: 'ei yhtään paikkaa vapaana', style: 'red'});
-        }
-    }, [parkingState.ID27, parkingState.ID20]);
-
-    // checks free spaces from parkingState object
-    useEffect(() => {
-        let freeTotalCount = 0;
-        for (const [, value] of Object.entries(parkingState)) {
-            if (!value) freeTotalCount++;
-        }
-        const freeNormalCount = freeTotalCount - freeInvaSpaces;
-        if (freeNormalCount >= 5) {
-            setFreeSpacesText({text: 'useita paikkoja vapaana', style: 'green'});
-        } else if (freeNormalCount >= 3) {
-            setFreeSpacesText({text: 'muutamia paikkoja vapaana', style: '#e7d213'});
-        } else if (freeNormalCount === 2) {
-            setFreeSpacesText({text: 'kaksi paikkaa vapaana', style: '#e7d213'});
-        } else if (freeNormalCount === 1) {
-            setFreeSpacesText({text: 'yksi paikka vapaana', style: '#e7d213'});
-        } else {
-            setFreeSpacesText({text: 'ei yhtään paikkaa vapaana', style: 'red'});
-        }
-        setTotalFreeSpaces(freeTotalCount);
-        setFreeNormalSpaces(freeNormalCount);
-        //setLoading(false);
-
-    }, [freeInvaSpaces, parkingState]);
-
-
-    // Every 5 seconds fetch parking data and set time now to state
-    useEffect(() => {
-
-        const timerInterval = setInterval(() => {
+        // Initial call for fetch functions
+        useEffect(() => {
             asyncFetch();
-            setTimeNow(Date.now()/1000);
-
-        }, 5000);
-
-        return () => clearInterval(timerInterval);
-    }, []);
-
-    // Check if sun has set
-    useEffect(()=> {
+            if (sunsetFuncOn) fetchSunset();
+            if (sunsetFuncOn) setTimeNow(Date.now() / 1000);
 
 
-        if (timeNow - sunset >= 0 && timeNow>= sunrise) {
-            setSunHasSet(true);
-        }else{
-            setSunHasSet(false)
-        }
+            const width = window.innerWidth;
 
-    },[timeNow, sunset])
+            if (width > 599) {
+                setScreenWidth(false);
+            }
+        }, []);
+
+
+        // Check if inva spaces are occupied and show them separately
+        useEffect(() => {
+            const list = [parkingState.ID20, parkingState.ID27];
+            const count = list.filter(item => !item).length;
+            setFreeInvaSpaces(count);
+
+            if (count === 2) {
+                setInvaSpacesText({text: 'kaksi paikkaa vapaana', style: 'green'});
+            } else if (count === 1) {
+                setInvaSpacesText({text: 'yksi paikka vapaana', style: '#e7d213'});
+            } else {
+                setInvaSpacesText({text: 'ei yhtään paikkaa vapaana', style: 'red'});
+            }
+        }, [parkingState.ID27, parkingState.ID20]);
+
+        // checks free spaces from parkingState object
+        useEffect(() => {
+            let freeTotalCount = 0;
+            for (const [, value] of Object.entries(parkingState)) {
+                if (!value) freeTotalCount++;
+            }
+            const freeNormalCount = freeTotalCount - freeInvaSpaces;
+            if (freeNormalCount >= 5) {
+                setFreeSpacesText({text: 'useita paikkoja vapaana', style: 'green'});
+            } else if (freeNormalCount >= 3) {
+                setFreeSpacesText({text: 'muutamia paikkoja vapaana', style: '#e7d213'});
+            } else if (freeNormalCount === 2) {
+                setFreeSpacesText({text: 'kaksi paikkaa vapaana', style: '#e7d213'});
+            } else if (freeNormalCount === 1) {
+                setFreeSpacesText({text: 'yksi paikka vapaana', style: '#e7d213'});
+            } else {
+                setFreeSpacesText({text: 'ei yhtään paikkaa vapaana', style: 'red'});
+            }
+            setTotalFreeSpaces(freeTotalCount);
+            setFreeNormalSpaces(freeNormalCount);
+            //setLoading(false);
+
+        }, [freeInvaSpaces, parkingState]);
+
+
+        // Every 5 seconds fetch parking data and set time now to state #sunsetfunc
+        //
+        //
+        // <-----------uncomment these to make sunset functionality work------------>
+        // useEffect(() => {
+        //
+        //     const timerInterval = setInterval(() => {
+        //         asyncFetch();
+        //         if(sunsetFuncOn) setTimeNow(Date.now()/1000);
+        //
+        //     }, 5000);
+        //
+        //     return () => clearInterval(timerInterval);
+        // }, []);
+
+        // Check if sun has set
+
+        // useEffect(() => {
+        //
+        //
+        //     if (timeNow - sunset >= 0 && timeNow >= sunrise) {
+        //         setSunHasSet(true);
+        //     } else {
+        //         setSunHasSet(false)
+        //     }
+        //
+        // }, [timeNow, sunset])
+        // <------------------------^^^^-------------------------------------------->
+
 
 
     // Fetch parking data
-    const asyncFetch = async () => {
-        let data = {};
+        const asyncFetch = async () => {
+            let data = {};
 
-        try {
-            const response = await fetch('/data/jsondata.json');
+            try {
+                const response = await fetch('/data/jsondata.json');
+                const responseData = await response.json();
+                data = responseData.response;
+                const obj = Object.values(data?.body);
+                const newObj = {};
+                obj.map(x => {
+                        newObj[`ID${x.id}`] = !!parseInt(x.status);
+                    },
+                );
+                setParkingState(newObj);
+            } catch (err) {
+                // Tänne fetchaus virheet
+                console.log('asyncFetch error', err.message);
+            }
+        };
+
+        // Fetch sunset data
+
+        const fetchSunset = async () => {
+            const response = await fetch('/data/sunsetInfo.json');
             const responseData = await response.json();
-            data = responseData.response;
-            const obj = Object.values(data?.body);
-            const newObj = {};
-            obj.map(x => {
-                    newObj[`ID${x.id}`] = !!parseInt(x.status);
-                },
-            );
-            setParkingState(newObj);
-        } catch (err) {
-            // Tänne fetchaus virheet
-            console.log('asyncFetch error', err.message);
+            console.log('koira', responseData);
+
+            setSunset(responseData?.sunset);
+            setSunrise(responseData?.sunrise);
+
         }
-    };
 
-    // Fetch sunset data
-    const fetchSunset = async () => {
-        const response = await fetch('/data/sunsetInfo.json');
-        const responseData = await response.json();
-        console.log('koira',responseData);
 
-        setSunset(responseData?.sunset);
-        setSunrise(responseData?.sunrise);
-
-    }
-
-    return (
-        <Container id={'container'} disableGutters={screenWidth} maxWidth={'md'}
-                   style={{display: 'flex', width: '100vw', height: '100vh'}}>
-            <ThemeProvider theme={theme}>
-                <BeenHereBeforeModal/>
-                {sunHasSet ? <SunsetChecker sunriseUnix={sunrise}/> :
+        return (
+            <Container id={'container'} disableGutters={screenWidth} maxWidth={'md'}
+                       style={{display: 'flex', width: '100vw', height: '100vh'}}>
+                <ThemeProvider theme={theme}>
+                    <BeenHereBeforeModal/>
                     <>
                         <Grid container>
                             <Grid item sm={6}>
@@ -277,12 +292,12 @@ const LandingPage = () => {
                             </Grid>
                         </Grid>
                     </>
-                }
-            </ThemeProvider>
-        </Container>
-    )
-        ;
-}
+
+                </ThemeProvider>
+            </Container>
+        )
+            ;
+    }
 ;
 
 export default LandingPage;
